6c4b328ebb629bdecfbba88a5a387b5e7613789b
NikitaKharitonov <nv.kharitonov@yandex.ru>
2020-09-12 17:54:29 +0400

    Unite run, listen and handle methods by one method in server.py and move some other game-related methods to game.py

--- game.py
+++ game.py
@@ -1,43 +1,149 @@
 import math
 from random import random
+import json
+
+import model
 
 NUMBER_OF_PLAYERS = 2
 MAX_INITIAL_DISTANCE = 10
 MIN_INITIAL_DISTANCE = 2
 MAX_ANGLE = 360
 
-initial_distance = MIN_INITIAL_DISTANCE + (MAX_INITIAL_DISTANCE - MIN_INITIAL_DISTANCE) * random()
+def init_distance():
+    """
+    Generates a random distance from the award for all players
+    in the [MIN_INITIAL_DISTANCE, MAX_INITIAL_DISTANCE) interval.
+
+    :return: the generated distance.
+    """
+    return MIN_INITIAL_DISTANCE + (MAX_INITIAL_DISTANCE - MIN_INITIAL_DISTANCE) * random()
 
 
-def recreate_player(username, client, x, y):
-    distance = math.sqrt(x ** 2 + y ** 2)
-    angle = math.atan2(y, x)
-    return Player(distance, angle, client, username)
+def create_player_from_polar_coordinates(username, distance, angle, client):
+    """
+    Converts the given polar coordinates into cartesian and
+    initializes a player.
+
+    :param username: the unique name of the player.
+    :param distance: the first polar coordinate.
+    :param angle: the second polar coordinate.
+    :param client: the client connection associated with the player.
+    :return: the player with the calculated cartesian coordinates.
+    """
+    angle = math.radians(angle)
+    x = distance * math.cos(angle)
+    y = distance * math.sin(angle)
+    return Player(username, x, y, client)
+
+
+def parse_data(data):
+    """
+    Parses the given dictionary to the list of Player objects.
+
+    :param data: the dictionary containing the Player data
+    :return: the list of Player objects.
+    """
+    players = list()
+    for player_data in data['players']:
+        player = Player(player_data['username'],
+                        player_data['x'],
+                        player_data['y'],
+                        None)
+        players.append(player)
+    return players
+
+
+def init_players():
+    """
+    Initializes players for the game so that the distance to the award
+    is equal for all of them.
+
+    :return: the two players as a list.
+    """
+    distance = init_distance()
+    players = list()
+    for i in range(0, 2):
+        players.append(
+            create_player_from_polar_coordinates(
+                f"player{i}", distance, MAX_ANGLE * random(), None
+            )
+        )
+    return players
+
+
+def load_game_state_from_json(json_file_path):
+    """
+    Loads the game state from the JSON file specified by the given path.
+
+    :param json_file_path: the JSON file path to load from.
+    :return: True when the game state has been successfully loaded,
+    False otherwise.
+    """
+    with open(json_file_path) as json_file:
+        try:
+            data = json.load(json_file)
+            if model.validate_json(data):
+                return True, data
+        except json.decoder.JSONDecodeError:
+            pass
+    return False, None
 
 
 class Player:
+    """
+    A player of the game.
+    The position of the player on the 2D game plane is described
+    by the (x, y) cartesian coordinates. The award is located in the origin
+    of the plane.
+    """
+    def __init__(self, username, x, y, client_socket):
+        """
+        Initializes a player.
 
-    def __init__(self, distance, angle, client, username=""):
-        angle = math.radians(angle)
-        self.x = distance * math.cos(angle)
-        self.y = distance * math.sin(angle)
-        self.client = client
+        :param username: the unique name of the player.
+        :param x: the first cartesian coordinate.
+        :param y: the second cartesian coordinate.
+        :param client_socket: the socket associated with the player.
+        """
         self.username = username
+        self.x = x
+        self.y = y
+        self.client_socket = client_socket
+
+    def __str__(self):
+        """
+        Converts the player to a string containing the username and the coordinates.
+
+        :return: the string representation of the player.
+        """
+        return f"Player username: {self.username}, x: {self.x}, y: {self.y}"
 
     def distance(self):
+        """
+        Calculates the distance to the award for the player.
+
+        :return: the distance.
+        """
         return math.sqrt(self.x ** 2 + self.y ** 2)
 
-    def make_move(self, angle):
+    def move(self, angle):
+        """
+        Moves the player one unit in the given angle from the OX direction.
+
+        :param angle: the angle.
+        """
         angle = math.radians(angle)
         x = math.cos(angle)
         y = math.sin(angle)
         self.x += x
         self.y += y
 
-    def __str__(self):
-        return f"Player name: {self.username}, x: {self.x}, y: {self.y}"
+    def dict(self):
+        """
+        Converts the player to a dictionary containing the username and the coordinates.
 
-    def json(self):
+        :return: the dictionary representation of the player.
+        """
         return {
             'username': self.username,
             'x': self.x,
--- server.py
+++ server.py
@@ -31,9 +31,103 @@ class Server(object):
         self.port = PORT
         self.socket = None
         self.new_game = True
-        self.game_state = {}
 
-    def listen(self):
+    # def listen(self):
+    #     self.socket.listen(1)
+    #     connected_clients_count = 0
+    #     while connected_clients_count != game.NUMBER_OF_PLAYERS:
+    #         try:
+    #             client, address = self.socket.accept()
+    #         except OSError:
+    #             print(CONNECTION_ABORTED)
+    #             return
+    #         print(CONNECTED_PATTERN.format(*address))
+    #
+    #         player = self.players[connected_clients_count]
+    #         player.client = client
+    #         client.sendall(Message(username=SERVER, message=player.username).marshal())
+    #
+    #         connected_clients_count += 1
+    #
+    #     if self.new_game:
+    #         message = f"GAME BEGIN! Distance to award: {round(game.initial_distance, 3)}"
+    #         self.broadcast(Message(username=SERVER, message=message))
+    #     else:
+    #         for player in self.players:
+    #             message = f"GAME CONTINUE! Distance to award: {round(player.distance(), 3)}"
+    #             player.client.sendall(Message(username=SERVER, message=message).marshal())
+    #
+    #     for player in self.players:
+    #         print(player)
+    #
+    #     self.dump_game_state_to_json()
+    #
+    #     while True:
+    #         for player in self.players:
+    #             client = player.client
+    #             message_from_server = Message(username=SERVER, message=MOVE_ALLOWED)
+    #             client.sendall(message_from_server.marshal())
+    #             try:
+    #                 message = Message(**json.loads(self.receive(client)))
+    #             except(ConnectionAbortedError, ConnectionResetError):
+    #                 print(CONNECTION_ABORTED)
+    #                 return
+    #             if message.quit:
+    #                 self.close_clients()
+    #                 return
+    #             angle = float(message.message)
+    #             player.make_move(angle)
+    #             message.message = f"angle: {angle % 360}, distance to award: {round(player.distance(), 3)}"
+    #             self.broadcast(message)
+    #             print(str(message))
+    #         for player in self.players:
+    #             print(player)
+    #         # fixme ?
+    #         if self.players[0].distance() <= 1 and self.players[1].distance() <= 1:
+    #             message = "dead heat!"
+    #             self.broadcast(Message(username=SERVER, message=message))
+    #             print(message)
+    #             self.close_clients()
+    #             self.players = list()
+    #             break
+    #         for player in self.players:
+    #             if player.distance() <= 1:
+    #                 message = f"{player.username} won!"
+    #                 self.broadcast(Message(username=SERVER, message=message))
+    #                 print(message)
+    #                 self.close_clients()
+    #                 self.players = list()
+    #                 return
+    #         self.dump_game_state_to_json()
+
+    def broadcast(self, message):
+        for player in self.players:
+            player.client_socket.sendall(message.marshal())
+
+    def receive(self, client):
+        buffer = ""
+        while not buffer.endswith(model.END_CHARACTER):
+            buffer += client.recv(BUFFER_SIZE).decode(model.TARGET_ENCODING)
+        return buffer[:-1]
+
+    def run(self):
+
+        # INITIALIZE
+
+        print(RUNNING)
+
+        success, dict = game.load_game_state_from_json(JSON_FILE_PATH)
+        if success:
+            self.players = game.parse_data(dict)
+        else:
+            self.players = game.init_players()
+        self.new_game = not success
+
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind(("", self.port))
+
+        # LISTEN
+
         self.socket.listen(1)
         connected_clients_count = 0
         while connected_clients_count != game.NUMBER_OF_PLAYERS:
@@ -44,42 +138,32 @@ class Server(object):
                 return
             print(CONNECTED_PATTERN.format(*address))
 
-            if self.new_game:
-                player = self.players[connected_clients_count]
-                player.client = client
-                # username = f"player{len(self.players)}"
-                client.sendall(Message(username=SERVER, message=player.username).marshal())
-                # player = Player(game.initial_distance, game.MAX_ANGLE * random(), client, username)
-                # self.players.append(player)
-            else:
-                # player_data = self.game_state['players'][len(self.players)]
-                # player = game.recreate_player(player_data['username'],
-                #                               client,
-                #                               player_data['x'],
-                #                               player_data['y'])
-                player = self.players[connected_clients_count]
-                player.client = client
-                # self.players.append(player)
-                client.sendall(Message(username=SERVER, message=player.username).marshal())
+            player = self.players[connected_clients_count]
+            player.client_socket = client
+            client.sendall(Message(username=SERVER, message=player.username).marshal())
+
             connected_clients_count += 1
 
         if self.new_game:
-            message = f"GAME BEGIN! Distance to award: {round(game.initial_distance, 3)}"
+            message = f"GAME BEGIN! Distance to award: {round(self.players[0].distance(), 3)}"
             self.broadcast(Message(username=SERVER, message=message))
         else:
             for player in self.players:
                 message = f"GAME CONTINUE! Distance to award: {round(player.distance(), 3)}"
-                player.client.sendall(Message(username=SERVER, message=message).marshal())
+                player.client_socket.sendall(Message(username=SERVER, message=message).marshal())
+
         for player in self.players:
             print(player)
+
         self.dump_game_state_to_json()
-        self.handle()
 
-    def handle(self):
+        # HANDLE
 
-        while True:
+        cont = True
+        while cont:
+
             for player in self.players:
-                client = player.client
+                client = player.client_socket
                 message_from_server = Message(username=SERVER, message=MOVE_ALLOWED)
                 client.sendall(message_from_server.marshal())
                 try:
@@ -88,108 +172,98 @@ class Server(object):
                     print(CONNECTION_ABORTED)
                     return
                 if message.quit:
-                    self.close_clients()
-                    return
+                    cont = False
+                    # self.close_clients()
+                    break
                 angle = float(message.message)
-                player.make_move(angle)
+                player.move(angle)
                 message.message = f"angle: {angle % 360}, distance to award: {round(player.distance(), 3)}"
                 self.broadcast(message)
                 print(str(message))
+
             for player in self.players:
                 print(player)
+
             # fixme ?
             if self.players[0].distance() <= 1 and self.players[1].distance() <= 1:
                 message = "dead heat!"
                 self.broadcast(Message(username=SERVER, message=message))
                 print(message)
-                self.close_clients()
                 self.players = list()
                 break
+
             for player in self.players:
                 if player.distance() <= 1:
                     message = f"{player.username} won!"
                     self.broadcast(Message(username=SERVER, message=message))
                     print(message)
-                    self.close_clients()
                     self.players = list()
-                    return
+                    cont = False
+                    break
+
             self.dump_game_state_to_json()
 
-    def broadcast(self, message):
-        for player in self.players:
-            player.client.sendall(message.marshal())
+        # CLOSE
 
-    def receive(self, client):
-        buffer = ""
-        while not buffer.endswith(model.END_CHARACTER):
-            buffer += client.recv(BUFFER_SIZE).decode(model.TARGET_ENCODING)
-        return buffer[:-1]
-
-    def run(self):
-        print(RUNNING)
-
-        success, data = self.load_game_state_from_json()
-        if success:
-            self.parse_data(data)
-        else:
-            self.initialize_players()
-        self.new_game = not success
-
-
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.bind(("", self.port))
-        self.listen()
-
+        self.close_clients()
         self.dump_game_state_to_json()
         print(CLOSING)
 
     def close_clients(self):
         self.socket.close()
         for player in self.players:
-            player.client.close()
+            player.client_socket.close()
         print(CLOSING)
 
     def dump_game_state_to_json(self):
         data = {'players': []}
         for player in self.players:
-            data['players'].append(player.json())
+            data['players'].append(player.dict())
         with open(JSON_FILE_PATH, 'w') as outfile:
             json.dump(data, outfile, indent=4)
 
-    def parse_data(self, data):
-        for player_data in data['players']:
-            player = game.recreate_player(player_data['username'],
-                                          None,
-                                          player_data['x'],
-                                          player_data['y'])
-            self.players.append(player)
-
-    def initialize_players(self):
-        self.players = (Player(game.initial_distance, game.MAX_ANGLE * random(), None, "player0"),
-                        Player(game.initial_distance, game.MAX_ANGLE * random(), None, "player1"))
-
-    def load_game_state_from_json(self):
-        """
-        Loads the game state from the JSON file specified by JSON_FILE_PATH
-        :return: True when the game state has been successfully loaded,
-        False otherwise
-        """
-        with open(JSON_FILE_PATH) as json_file:
-            try:
-                data = json.load(json_file)
-                if model.validate_json(data):
-                    # self.new_game = False
-                    self.game_state = data
-                    # for player_data in data['players']:
-                    #     player = game.recreate_player(player_data['username'],
-                    #                                   None,
-                    #                                   player_data['x'],
-                    #                                   player_data['y'])
-                    #     self.players.append(player)
-                    return True, data
-            except json.decoder.JSONDecodeError:
-                pass
-        return False, None
+    # def parse_data(self, dict):
+    #     """
+    #     Parses the given dictionary to the list of Player objects
+    #
+    #     :param dict: the dictionary containing the Player data
+    #     :return: the list of Player objects
+    #     """
+    #     players = list()
+    #     for player_data in dict['players']:
+    #         player = game.recreate_player(player_data['username'],
+    #                                       None,
+    #                                       player_data['x'],
+    #                                       player_data['y'])
+    #         players.append(player)
+    #     return players
+    #
+    # def initialize_players(self):
+    #     """
+    #     Initializes two players for the game so that the distance to the award
+    #     is equal for both of them
+    #
+    #     :return: the two players as a list
+    #     """
+    #     return [Player(game.initial_distance, game.MAX_ANGLE * random(), None, "player0"),
+    #             Player(game.initial_distance, game.MAX_ANGLE * random(), None, "player1")]
+    #
+    # def load_game_state_from_json(self, json_file_path):
+    #     """
+    #     Loads the game state from the JSON file specified by the given path
+    #
+    #     :param json_file_path: the JSON file path to load from
+    #     :return: True when the game state has been successfully loaded,
+    #     False otherwise
+    #     """
+    #     with open(json_file_path) as json_file:
+    #         try:
+    #             data = json.load(json_file)
+    #             if model.validate_json(data):
+    #                 return True, data
+    #         except json.decoder.JSONDecodeError:
+    #             pass
+    #     return False, None
 
 
 if __name__ == "__main__":
